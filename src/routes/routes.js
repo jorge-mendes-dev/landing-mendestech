31c18e20f24bef4aaa8195479cb96f1f1063e4a1
Jorge Silva <jorge.silva>
2022-04-23 16:42:45 -0300

    feat(about page): about page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import history from 'utils/history'
+
 import Page404 from 'pages/Page404'
+import About from 'pages/about'
 import Home from 'pages/home'
 
 const PageRoutes = () => (
@@ -9,6 +11,7 @@ const PageRoutes = () => (
     <Suspense fallback={false}>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Suspense>
