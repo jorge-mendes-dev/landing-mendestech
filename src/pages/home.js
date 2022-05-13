720789f6e7b38686ba39783aa8ce581f6c718209
Jorge Mendes <jorge.mendes@escala-app.com>
2022-05-12 21:14:12 -0300

    feat(using .env): using .env

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,12 @@ import { Seo } from 'components'
 
 function Home() {
   const meta = {
-    title: process.env.TITLE || '',
-    description: process.env.DESCRIPTION || '',
-    lang: process.env.LANG || '',
-    image: process.env.IMAGE || '',
-    author: process.env.AUTHOR || '',
-    siteUrl: process.env.SITE_URL || ''
+    title: process.env.REACT_APP_TITLE || '',
+    description: process.env.REACT_APP_DESCRIPTION || '',
+    lang: process.env.REACT_APP_LANG || '',
+    image: process.env.REACT_APP_IMAGE || '',
+    author: process.env.REACT_APP_AUTHOR || '',
+    siteUrl: process.env.REACT_APP_SITE_URL || ''
   }
 
   return (
