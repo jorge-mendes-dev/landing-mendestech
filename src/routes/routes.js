cc469f38ff483bda867a1589ca72e6b94848e3cf
jorgeMendes-escala <jorge.mendes@escala-app.com>
2023-04-04 19:04:10 -0300

    utils link

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,7 +10,7 @@ const PageRoutes = () => (
     <Suspense fallback={""}>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/utils" element={<Utils />} />
+        <Route exact path="utils" element={<Utils />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Suspense>
