696dfc7b9558eed48acca3434a478292bdefa087
jorgeMendes-escala <jorge.mendes@escala-app.com>
2023-04-05 16:47:13 -0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontSize: {
       xs: '0.75rem',
