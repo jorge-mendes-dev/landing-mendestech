27e8e97b8c5d2c30f1833f58f96ba96aacfae4f9
jorgeMendes-escala <jorge.mendes@escala-app.com>
2023-11-17 19:11:55 -0300

    optmize app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      test: /\.(png|jpe?g|gif|webp|avif)$/,
+      exclude: /node_modules/,
+      include: ['src/images'],
+      logStats: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false
+              },
+              cleanupIDs: {
+                minify: false,
+                remove: false
+              },
+              convertPathData: false
+            }
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'https://www.w3.org/2000/svg' }]
+            }
+          }
+        ]
+      },
+      jpeg: {
+        quality: 100
+      },
+      cache: true,
+      cacheLocation: 'node_modules/.vite-plugin-image-optimizer-cache'
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
@@ -17,5 +57,9 @@ export default defineConfig({
       utils: path.resolve(__dirname, './src/utils'),
       routes: path.resolve(__dirname, './src/routes')
     }
+  },
+  optimizeDeps: {
+    include: ['esm-dep > cjs-dep'],
+    force: true
   }
 })
