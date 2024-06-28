3b1ee0c6fd2d2eadabf400fa913ad0bc193f3c56
jorgeMendes-escala <jorge.mendes@escala-app.com>
2024-06-28 16:51:21 -0300

    updates on website design - general

--- src/components/SkillsList/index.jsx
+++ src/components/SkillsList/index.jsx
@@ -43,7 +43,7 @@ const SkillsList = ({ ...props }) => {
               className="w-24 md:w-32 min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-xl group relative hover:scale-[1.15] cursor-pointer"
               key={random()}
             >
-              <div className="h-full w-full rounded-xl border border-ternary-light bg-white shadow shadow-gray-50 group-hover:shadow-xl group-hover:border-primary transition-all duration-300">
+              <div className="h-full w-full rounded-xl border border-ternary-light bg-white shadow shadow-gray-50 group-hover:shadow-xl group-hover:bg-blue-50 transition-all duration-300">
                 <div className="flex -translate-y-[1px] justify-center">
                   <div className="w-3/4">
                     <div className="h-[1px] w-full bg-gradient-to-r from-transparent bg-primary to-transparent" />
@@ -54,8 +54,8 @@ const SkillsList = ({ ...props }) => {
                     <img
                       alt={skill.title}
                       src={Techs[skill.img]}
-                      width={40}
-                      height={40}
+                      width={30}
+                      height={30}
                       className="h-full w-auto rounded-xl"
                     />
                   </div>
